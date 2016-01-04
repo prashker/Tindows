06ca78f60f56d6e512cf477f6405dd74e138ba69
Samuel Prashker <github@prashker.net>
2016-01-04 02:27:31 -0500

    First commit - Template10 boilerplate + Preliminary FB Auth
    This first commit introduces the groundwork for the project. This means the inclusion of "Template10" as the framework of choice. Given that Universal Apps and XAML is new to me. I figured a pre-existing wrapper that abstracts a little bit of the nitty gritty to be useful.
    
    I've implemented a test LoginPage that calls FB to retrieve an authorization token. This code will need to be moved to the model in the future.

---
+++ ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.UI.Xaml.Navigation;
+
+namespace Tindows.ViewModels
+{
+    public class LoginPageViewModel : Mvvm.ViewModelBase
+    {
+        public LoginPageViewModel()
+        {
+        }
+
+
+        // Per http://stackoverflow.com/questions/12144077/async-await-when-to-return-a-task-vs-void
+        // The main exception should be when you need to have a void return type (for events). 
+        
+        public async void facebookLogin()
+        {
+            var app_id = "464891386855067";
+            var scope = "public_profile, email";
+
+            // Generate URL
+            var callbackUrl = "https://www.facebook.com/connect/login_success.html";
+            var url = "https://www.facebook.com/dialog/oauth?client_id=" + app_id +
+                        "&redirect_uri=https://www.facebook.com/connect/login_success.html" +
+                        "&scope=" + scope + "&display=popup&response_type=token";
+
+            Uri startUri = new Uri(url);
+            Uri endUri = new Uri(callbackUrl);
+
+            WebAuthenticationResult result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, endUri);
+            // Example response:
+            // result.ReponseData = "https://www.facebook.com/connect/login_success.html#access_token=[PARSE-FOR-THIS]&expires_in=5754"
+        }
+    }
+}
---
+++ Views/LoginPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Tindows.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Tindows.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // strongly-typed view models enable x:bind
+        public LoginPageViewModel ViewModel => this.DataContext as LoginPageViewModel;
+    }
+}

e0660f87f302e16981c1df66d1227f2e7d785fa0
Samuel Prashker <github@prashker.net>
2016-02-19 22:11:15 -0500

    Tapping the contact on the SuperficialPage now navigates to the Bio tab

--- Views/SuperficialPage.xaml.cs
+++ Views/SuperficialPage.xaml.cs
@@ -15,5 +15,11 @@ namespace Tindows.Views
 
         // strongly-typed view models enable x:bind
         public SuperficialPageViewModel ViewModel => this.DataContext as SuperficialPageViewModel;
+
+        // When you click/tap the contact, go to the Info Page
+        private void YesOrNoDisplay_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            SuperficialPivot.SelectedIndex = 1;
+        }
     }
 }
